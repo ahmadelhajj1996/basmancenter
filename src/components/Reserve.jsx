106627289b948f39da3261a402fa9f1d3b685d40
Ahmad Elhaj <139491035+ahmadelhajj1996@users.noreply.github.com>
2026-02-09 18:15:52 +0400

    Update Reserve.jsx

--- src/components/Reserve.jsx
+++ src/components/Reserve.jsx
@@ -69,7 +69,6 @@ function Register() {
     `*📧 Email:* ${values.email}%0A` +
     `*📅 Date:* ${formattedDate}%0A` +
     `*⏰ Time:* ${formattedTime}%0A%0A` +
-    `_This appointment was booked via Basman Alnuaini medical center_`;
 
   // Clean phone number
   const phoneNumber = "971508149362".replace(/\D/g, "");
@@ -234,3 +233,4 @@ function Register() {
 }
 
 export default Register;
+
