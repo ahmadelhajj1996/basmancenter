ff52b0da5705e22079297823fe6c3526bb5c5a9e
Ahmad Elhaj <139491035+ahmadelhajj1996@users.noreply.github.com>
2026-02-09 18:11:44 +0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,23 @@
+import { useEffect } from "react";
+import { useTranslation } from "react-i18next";
 import AppRoutes from "./config/router";
-import Backtop from "./components/Backtop";
-
 
 function App() {
-  
+  const { i18n } = useTranslation();
+
+  useEffect(() => {
+    // Check and set direction on initial render
+    const currentLang = i18n.language || localStorage.getItem("i18nextLng") || "en";
+    document.dir = currentLang === "ar" ? "rtl" : "ltr";
+  }, [i18n]);
 
   return (
     <>
-      <div className="m-0 w-full min-h-screen transform-none filter-none" >
+      <div className="m-0 w-full min-h-screen transform-none filter-none">
         <AppRoutes />
       </div>
-      <Backtop />
     </>
   );
 }
 
-export default App;
+export default App; 
